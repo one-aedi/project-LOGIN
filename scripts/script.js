aaa93e51f905a2ac4760d2f95f0c7c1bcda87415
one-aedi <adrian.dzurovcin@gmail.com>
2021-11-07 21:26:29 +0100

    version1.2 - jqueryui for form-box

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
 $(function(){
 
-var pozadie = $(".form-box");
+var pozadie = $(".hero");
+var tabulka = $(".form-box")
 
-pozadie.hide().delay(2000).show("fade", 1000);
+pozadie.hide().delay(3000).show("fade", 2000);
+tabulka.hide().delay(4000).show("clip", 1000);
 
 
+
+setTimeout(function() {
+    $('body')
+      .removeClass('loading')
+      .addClass('loaded');
+  }, 3000);
 
 
 });
